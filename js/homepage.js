e1c80ab5b31f69e4cfb1768070792f9fe70bc037
Auréliane <aureliane.gailliegue@gmail.com>
2021-10-11 19:56:50 +0200

    Création de tous les photographes sur la homepage (sans responsive)

--- js/homepage.js
+++ js/homepage.js
@@ -1,13 +1,8 @@
 // ========== DOM ELEMENTS ==========
 
-const main = document.querySelector("main");
+const nav = document.querySelector(".nav");
+const photographersList = document.querySelector(".photographers_list");
 const photographerCard = document.querySelector(".photographer_card");
-const photographerCardProfilePicture = document.querySelector(".photographer_card_profilepicture");
-const photographerCardName = document.querySelector(".photographer_card_name");
-const photographerCardLocation = document.querySelector(".photographer_card_location");
-const photographerCardDescription = document.querySelector(".photographer_card_description");
-const photographerCardPrice = document.querySelector(".photographer_card_price");
-const photographerCardTags = document.querySelector(".photographer_card_tags");
 
 
 // ========== FUNCTIONS ==========
@@ -29,26 +24,63 @@ fetch(jsonUrl)
 // Save global variables after fetch operation
 
 setTimeout(() => {
+
     let data = window.data;
     let photographers = data.photographers;
     let media = data.media;
-    console.log("data", data);
-    console.log("photographers", photographers);
-    console.log("media", media);
-
     let nbPhotographers = photographers.length;
+    //console.log("data", data);
+    //console.log("photographers", photographers);
+    //console.log("media", media);
+    //console.log("nbPhotographers", nbPhotographers);
 
-    for (i = 0; i < nbPhotographers; i++) {
-        if (i = 0) {
-            console.log("test");
+    // Initialize empty navigation filters
+    const navTags = [];
+
+    for (let i = 0; i < nbPhotographers; i++) {
+
+        // Create a clone of the original photographer card
+        if (i != 0) {
+            photographerCardNew = photographerCard.cloneNode(true);
+            photographersList.appendChild(photographerCardNew);
         }
-        console.log("i");
-        console.log(photographers[i]);
-        //photographerCardNew = photographerCard.cloneNode(true);
-        //main.appendChild(photographerCardNew);
 
-        console.log("Done");
+        // Get last photographer card created
+        const photographerCards = document.querySelectorAll(".photographer_card");
+
+        // Get all HTML children
+        [photographerCardProfilePicture, photographerCardName, photographerCardLocation, photographerCardDescription, photographerCardPrice, photographerCardTags] = photographerCards[i].children;
+        
+        // Change text in HTML by data in JSON
+        photographerCardProfilePicture.src = "./../assets/pictures/photographers/" + photographers[i].portrait;
+        photographerCardName.innerHTML = photographers[i].name;
+        photographerCardLocation.innerHTML = photographers[i].city + ', ' + photographers[i].country;
+        photographerCardDescription.innerHTML = photographers[i].tagline;
+        photographerCardPrice.innerHTML = photographers[i].price + " € / jour";
+        // Empty tags
+        photographerCardTags.innerHTML = '';
+        for (let j = 0; j < photographers[i].tags.length; j++) {
+            // Create a new div for each tag
+            const photographerCardTag = document.createElement("div");
+            photographerCardTag.innerHTML = "#" + photographers[i].tags[j];
+            photographerCardTags.appendChild(photographerCardTag);
+            // Check if the tag is already in the navigation bar
+            const navTagName = "#" + photographers[i].tags[j].charAt(0).toUpperCase() + photographers[i].tags[j].slice(1);
+            if (navTags.includes(navTagName) == false) {
+                navTags.push(navTagName);
+            }
+        }
     }
+
+    // Create navigation filters
+    for (let k = 0; k < navTags.length; k++) {
+        const navTag = document.createElement("div");
+        navTag.innerHTML = navTags[k];
+        nav.appendChild(navTag);
+    }
+
+    console.log("All done!");
+
 }, 500);
 
 
